46cb9eb228d9a1fe8899e90506ad268029ca09f3
pidgezero <pidgezero.one@gmail.com>
2018-01-29 17:34:13 -0500

    oops
    Signed-off-by: pidgezero <pidgezero.one@gmail.com>

---
+++ QueueObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunaIntegration
+{
+    //queue object from IRC to Discord
+    class QueueObject : ICloneable
+    {
+        public string type;
+        public Int32 timestamp;
+        public string channel;
+        public string text;
+        public string sender;
+        public UInt64 associatedId;
+
+        public QueueObject(String type, Int32 timestamp, string channel, string text, string sender, UInt64 associatedId)
+        {
+            this.type = type;
+            this.timestamp = timestamp;
+            this.channel = channel;
+            this.text = text;
+            this.sender = sender;
+            this.associatedId = associatedId;
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
